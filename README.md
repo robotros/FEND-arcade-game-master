7cbc7f318e2c69873f169fa854f0eefc00dc273d
Aron Roberts <robotros1@msn.com>
2018-08-08 23:56:29 -0700

    Docs: readme updated

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# fend-arcade-game-master
+**Table of contents**
+
+
+# **fend-arcade-game-master**
+## Technical Instructions
+### Description
+Simple Arcade Game made using Javascript
+### what is included
+* js\app.js
+* js\engine.js
+* js\resources.js
+* index.html
+* css\style.css
+* \images
+
+### Dependencies
+* Javascript enabled webbrowser
+
+### Quickstart
+* open index.html
+
+## Game Instructions
+### How to Play
+Move the player using the directional pad on the keyboard.
+### Objective
+The object of the game is to get to the water with being hit by a bug.
+When the Goal is achieved the Player will respawn at the start.
+### Losing
+When a bug comes into contact with a player the player loses.
+When a player loses he we respawn at the begining.
--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+/* app.js
+ *
+ */
 const rows = 5;
 const columns = 5;
 const canvasWidth = 505;
-const movement=canvasWidth/columns;
+const movement = canvasWidth / columns;
 const yMovement = 83;
 
 
@@ -13,10 +16,10 @@ const yMovement = 83;
  */
 var Enemy = function(speed, row) {
 
-	// x coordinate for sprite
-    this.x = -((Math.floor(Math.random() * 10)/2)+1);
+    // x coordinate for sprite
+    this.x = -((Math.floor(Math.random() * 10) / 2) + 1);
     // y coordinate for sprite
-    row ? this.y = (yMovement*row)-15 : this.y = yMovement-15;
+    row ? this.y = (yMovement * row) - 15 : this.y = yMovement - 15;
     // enemy speed
     speed ? this.speed = speed : this.speed = 4;
     // The image/sprite for our enemies, this uses
@@ -28,15 +31,15 @@ var Enemy = function(speed, row) {
  * @param dt - a time delta between ticks
  */
 Enemy.prototype.update = function(dt) {
-    
+
     // check canvas boundary
     if (this.x > canvasWidth) {
-    	r = (Math.floor(Math.random() * 10)/2)+1
-    	r1 = Math.floor(Math.random() * 10)+1
-        this.x= -movement*(r);
+        r = (Math.floor(Math.random() * 10) / 2) + 1
+        r1 = Math.floor(Math.random() * 10) + 1
+        this.x = -movement * (r);
         this.speed = r1;
     } else {
-        this.x = this.x+(dt*this.speed*(movement/4));
+        this.x = this.x + (dt * this.speed * (movement / 4));
     }
 };
 
@@ -49,29 +52,29 @@ Enemy.prototype.render = function() {
  * Player of the game
  * @constructor
  */
-var Player = function(){
-	// player sprite
+var Player = function() {
+    // player sprite
     this.sprite = 'images/char-boy.png';
     // x coordinate of sprite
-    this.x = 2*movement;
+    this.x = 2 * movement;
     // y coordinate of sprite
-    this.y=(rows*yMovement)-10;
+    this.y = (rows * yMovement) - 10;
 };
 
 /* checks player location for win/lose conditions */
-Player.prototype.update = function(){
-	//check win condition
-	if(this.y===-10){
-		this.x = 2*movement;
-		this.y = (rows*yMovement)-10;
-	};
-	//check lose condition
-	allEnemies.forEach(function(each){	
-		if (each.y+5===player.y && each.x > player.x-(movement-30) && each.x<player.x+movement){
-			player.x = 2*movement;
-			player.y = (rows*yMovement)-10;	
-		};
-	});
+Player.prototype.update = function() {
+    //check win condition
+    if (this.y === -10) {
+        this.x = 2 * movement;
+        this.y = (rows * yMovement) - 10;
+    };
+    //check lose condition
+    allEnemies.forEach(function(each) {
+        if (each.y + 5 === player.y && each.x > player.x - (movement - 30) && each.x < player.x + movement) {
+            player.x = 2 * movement;
+            player.y = (rows * yMovement) - 10;
+        };
+    });
 };
 
 /* Draw the player on the screen */
@@ -83,26 +86,26 @@ Player.prototype.render = function() {
  * move player according to input
  * @param keycode - input movement direction
  */
-Player.prototype.handleInput = function(keycode){
-    switch (keycode){
+Player.prototype.handleInput = function(keycode) {
+    switch (keycode) {
         case 'left':
             if (this.x >= movement) {
-            	this.x = this.x-movement;
+                this.x = this.x - movement;
             }
             break;
         case 'right':
-            if (this.x < canvasWidth-movement) {
-            	this.x = this.x+movement;
-        	}
+            if (this.x < canvasWidth - movement) {
+                this.x = this.x + movement;
+            }
             break;
         case 'up':
             if (this.y > 0) {
-            	this.y=this.y-yMovement;
-        	}
+                this.y = this.y - yMovement;
+            }
             break;
         case 'down':
-            if (this.y < (yMovement*(rows))-10) {
-                this.y=this.y+yMovement;
+            if (this.y < (yMovement * (rows)) - 10) {
+                this.y = this.y + yMovement;
             }
             break;
     }
@@ -114,21 +117,21 @@ let player = new Player();
 let allEnemies = [];
 
 // Place all enemy objects in an array called allEnemies
-for (x = 0; x < 2; x++){
-for(i = 1; i <= rows-2; i++) {
-    let speed = Math.floor(Math.random() * 10)+1;
-    let enemy = new Enemy(speed,i);
-    allEnemies.push(enemy);
-};
+for (x = 0; x < 2; x++) {
+    for (i = 1; i <= rows - 2; i++) {
+        let speed = Math.floor(Math.random() * 10) + 1;
+        let enemy = new Enemy(speed, i);
+        allEnemies.push(enemy);
+    };
 };
 
 
 /* 
-* This listens for key presses and sends the keys to your
-* Player.handleInput() method. You don't need to modify this.
-*
-* @param keyup - input from keyboard
-*/
+ * This listens for key presses and sends the keys to your
+ * Player.handleInput() method. You don't need to modify this.
+ *
+ * @param keyup - input from keyboard
+ */
 
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
