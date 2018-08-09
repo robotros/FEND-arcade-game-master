35da263a545f4ad942cf0824b5f1fb07492d2f06
Aron Roberts <robotros1@msn.com>
2018-08-09 12:15:55 -0700

    refactor:prettfy

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ const canvasWidth = 505;
 const movement = canvasWidth / columns;
 const yMovement = 83;
 
-
 /**
  * Enemies our player must avoid
  * @constructor
@@ -16,15 +15,14 @@ const yMovement = 83;
  * @param row - Trow the enemy will spawn on
  */
 var Enemy = function(speed, row) {
-
     // x coordinate for sprite
-    this.x = -((Math.floor(Math.random() * 10) / 2) + 1);
+    this.x = -(Math.floor(Math.random() * 10) / 2 + 1);
     // y coordinate for sprite
-    row ? this.y = (yMovement * row) - 15 : this.y = yMovement - 15;
+    row ? (this.y = yMovement * row - 15) : (this.y = yMovement - 15);
     // enemy speed
-    speed ? this.speed = speed : this.speed = 4;
+    speed ? (this.speed = speed) : (this.speed = 4);
     // The image/sprite for our enemies, this uses
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = "images/enemy-bug.png";
 };
 
 /**
@@ -32,15 +30,14 @@ var Enemy = function(speed, row) {
  * @param dt - a time delta between ticks
  */
 Enemy.prototype.update = function(dt) {
-
     // check canvas boundary
     if (this.x > canvasWidth) {
-        let r = (Math.floor(Math.random() * 10) / 2) + 1;
+        let r = Math.floor(Math.random() * 10) / 2 + 1;
         let r1 = Math.floor(Math.random() * 10) + 1;
-        this.x = -movement * (r);
+        this.x = -movement * r;
         this.speed = r1;
     } else {
-        this.x = this.x + (dt * this.speed * (movement / 4));
+        this.x = this.x + dt * this.speed * (movement / 4);
     }
 };
 
@@ -55,11 +52,11 @@ Enemy.prototype.render = function() {
  */
 var Player = function() {
     // player sprite
-    this.sprite = 'images/char-boy.png';
+    this.sprite = "images/char-boy.png";
     // x coordinate of sprite
     this.x = 2 * movement;
     // y coordinate of sprite
-    this.y = (rows * yMovement) - 10;
+    this.y = rows * yMovement - 10;
 };
 
 /* checks player location for win/lose conditions */
@@ -67,15 +64,21 @@ Player.prototype.update = function() {
     //check win condition
     if (this.y === -10) {
         this.x = 2 * movement;
-        this.y = (rows * yMovement) - 10;
-    };
+        this.y = rows * yMovement - 10;
+    }
     //check lose condition
-    allEnemies.forEach(function(each) {
-        if (each.y + 5 === this.y && each.x > this.x - (movement - 30) && each.x < this.x + (movement-30)) {
-            this.x = 2 * movement;
-            this.y = (rows * yMovement) - 10;
-        };
-    }.bind(this));
+    allEnemies.forEach(
+        function(each) {
+            if (
+                each.y + 5 === this.y &&
+                each.x > this.x - (movement - 30) &&
+                each.x < this.x + (movement - 30)
+            ) {
+                this.x = 2 * movement;
+                this.y = rows * yMovement - 10;
+            }
+        }.bind(this)
+    );
 };
 
 /* Draw the player on the screen */
@@ -89,29 +92,28 @@ Player.prototype.render = function() {
  */
 Player.prototype.handleInput = function(keycode) {
     switch (keycode) {
-        case 'left':
+        case "left":
             if (this.x >= movement) {
                 this.x = this.x - movement;
             }
             break;
-        case 'right':
+        case "right":
             if (this.x < canvasWidth - movement) {
                 this.x = this.x + movement;
             }
             break;
-        case 'up':
+        case "up":
             if (this.y > 0) {
                 this.y = this.y - yMovement;
             }
             break;
-        case 'down':
-            if (this.y < (yMovement * (rows)) - 10) {
+        case "down":
+            if (this.y < yMovement * rows - 10) {
                 this.y = this.y + yMovement;
             }
             break;
     }
 };
-
 
 // Place the player object in a variable called player
 const player = new Player();
@@ -123,9 +125,8 @@ for (let x = 0; x < 2; x++) {
         let speed = Math.floor(Math.random() * 10) + 1;
         let enemy = new Enemy(speed, i);
         allEnemies.push(enemy);
-    };
-};
-
+    }
+}
 
 /*
  * This listens for key presses and sends the keys to your
@@ -134,12 +135,12 @@ for (let x = 0; x < 2; x++) {
  * @param keyup - input from keyboard
  */
 
-document.addEventListener('keyup', function(e) {
+document.addEventListener("keyup", function(e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
